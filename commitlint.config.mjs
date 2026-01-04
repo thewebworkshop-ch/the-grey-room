04af6a3559a605a9b258a75e9f416358db0a21e6
Francesco Carnevale <dev@thewebworkshop.ch>
2026-01-04 18:46:16 +0100

    ci: integrate SonarCloud code analysis

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -5,19 +5,19 @@ const config = {
       2,
       "always",
       [
-        "feat", // Nouvelle fonctionnalité
-        "fix", // Correction de bug
-        "docs", // Documentation uniquement
-        "style", // Formatage (pas de changement de code)
-        "refactor", // Refactorisation (ni feat ni fix)
-        "perf", // Amélioration de performance
-        "test", // Ajout/modification de tests
-        "chore", // Maintenance (dépendances, config)
-        "ci", // Changements CI/CD
-        "revert", // Revert d'un commit précédent
+        "feat",
+        "fix",
+        "docs",
+        "style",
+        "refactor",
+        "perf",
+        "test",
+        "chore",
+        "ci",
+        "revert",
       ],
     ],
-    "subject-case": [0], // Désactive la règle de casse (permet toute casse)
+    "subject-case": [0],
   },
 };
 
