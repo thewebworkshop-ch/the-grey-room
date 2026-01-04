a1b9cae26c97f47d937313ab2c85d606de45488b
Francesco Carnevale <dev@thewebworkshop.ch>
2026-01-04 10:24:17 +0100

    chore: extend lint-staged to include mjs/cjs/yaml/json/md files

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -1,22 +1,24 @@
-export default {
-  extends: ['@commitlint/config-conventional'],
+const config = {
+  extends: ["@commitlint/config-conventional"],
   rules: {
-    'type-enum': [
+    "type-enum": [
       2,
-      'always',
+      "always",
       [
-        'feat',     // Nouvelle fonctionnalité
-        'fix',      // Correction de bug
-        'docs',     // Documentation uniquement
-        'style',    // Formatage (pas de changement de code)
-        'refactor', // Refactorisation (ni feat ni fix)
-        'perf',     // Amélioration de performance
-        'test',     // Ajout/modification de tests
-        'chore',    // Maintenance (dépendances, config)
-        'ci',       // Changements CI/CD
-        'revert',   // Revert d'un commit précédent
+        "feat", // Nouvelle fonctionnalité
+        "fix", // Correction de bug
+        "docs", // Documentation uniquement
+        "style", // Formatage (pas de changement de code)
+        "refactor", // Refactorisation (ni feat ni fix)
+        "perf", // Amélioration de performance
+        "test", // Ajout/modification de tests
+        "chore", // Maintenance (dépendances, config)
+        "ci", // Changements CI/CD
+        "revert", // Revert d'un commit précédent
       ],
     ],
-    'subject-case': [0], // Désactive la règle de casse (permet toute casse)
+    "subject-case": [0], // Désactive la règle de casse (permet toute casse)
   },
 };
+
+export default config;
